92d34e866212f23e72cbdc4f295375ce465dc243
Abhay  Dubey <146931800+abhaydubey200@users.noreply.github.com>
2026-01-03 11:57:09 +0530

    Create uploader.py

---
+++ data_engine/uploader.py
@@ -0,0 +1,10 @@
+# data_engine/uploader.py
+import pandas as pd
+
+def load_dataset(uploaded_file):
+    if uploaded_file.name.endswith(".csv"):
+        return pd.read_csv(uploaded_file)
+    elif uploaded_file.name.endswith(".xlsx"):
+        return pd.read_excel(uploaded_file)
+    else:
+        raise ValueError("Unsupported file format")
